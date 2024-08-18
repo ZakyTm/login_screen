88aaa12adf7f4a74b3c9f9af6818a68490d573c0
ZakyTm <zakaria.t149919@gmail.com>
2024-08-18 07:46:37 +0100

    Add necessary project files for Linux, macOS, Windows, and Android platforms

---
+++ lib/screens/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(
+            'Welcome BAck',
+          ),
+        ],
+      ),
+    );
+  }
+}
